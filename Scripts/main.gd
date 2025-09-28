ddaf596c408a5c3c3ae4f6c3f917733e2664ebde
Xavier Amico <christianxamico@gmail.com>
2025-09-27 22:48:30 -0500

    Hid HUD on Main Menu

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,14 +3,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	HUD.visible = true
-	var player = $Player
-	HUD.connect_to_player(player)
+	HUD.hide()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var player = $Player
-	player.take_damage(3)
 	pass
