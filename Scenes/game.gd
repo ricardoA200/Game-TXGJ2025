7a2c185ee7e036df662a36976e0599eb0222464d
Xavier Amico <christianxamico@gmail.com>
2025-09-28 15:23:25 -0500

    Added lava and some level changes

--- Scenes/game.gd
+++ Scenes/game.gd
@@ -1,16 +1,22 @@
 extends Node2D
 
+@onready var player = $Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	HUD.visible = true
-	var player = $Player
 	HUD.connect_to_player(player)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var player = $Player
-	player.take_damage(3)
 	pass
+
+
+func _on_lava_body_entered(body):
+	if body == player:
+		player.take_damage(100)
+		print("awwww it burns")
+	print("lava entered")
+	pass # Replace with function body.
